791a1836614f05936f22a0e7b024c99d401cc6c8
LinkedDestiny <simonarthur2012@gmail.com>
2016-11-27 00:41:06 +0800

    fix

---
+++ app/api/Test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lancelot
+ * Date: 16-11-27
+ * Time: 上午12:39
+ */
+
+namespace api;
+
+class Test
+{
+
+    public function ModelTest()
+    {
+
+    }
+
+    public function RedisTest()
+    {
+
+    }
+
+    public function HttpTest()
+    {
+
+    }
+
+    public function TaskTest()
+    {
+
+    }
+}
